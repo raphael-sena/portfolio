1379a7a9cf7510618ceacfa5cc1ccc3bea648b76
raphael sena <rsenares1@gmail.com>
2024-12-23 20:09:52 -0300

    feat: add theme toggle dark or light

---
+++ code/components/ThemeToggle.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState<string>('light');
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    setTheme(savedTheme);
+    document.documentElement.classList.add(savedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+
+    document.documentElement.classList.remove(theme);
+    document.documentElement.classList.add(newTheme);
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 shadow-md bg-black text-slate-50 dark:bg-slate-50 dark:text-black rounded-md text-sm"
+    >
+      {theme === 'light' ? '🌙 Dark Mode' : '☀️ Light Mode'}
+    </button>
+  );
+};
+
+export default ThemeToggle;
