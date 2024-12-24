6b1d5a86a6e7faca2c4405396f966abde7aac584
raphael sena <rsenares1@gmail.com>
2024-12-23 21:41:15 -0300

    feat: add metadata for sharing

--- code/app/metadata.tsx
+++ code/app/metadata.tsx
@@ -1,6 +1,33 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Raphael Sena | Portfólio",
-  description: "Portfólio de desenvolvimento",
+  title: "Raphael Sena | Portfolio",
+  description: "Dev Portfolio",
+  openGraph: {
+    title: "Raphael Sena | Portfolio",
+    description: "Dev Portfolio",
+    url: "https://raphaelsena.com",
+    type: "website",
+    images: [
+      {
+        url: "/images/avatar.png",
+        width: 630,
+        height: 630,
+        alt: "Raphael Sena Portfolio Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Raphael Sena | Portfolio",
+    description: "Dev Portfolio",
+    images: [
+      {
+        url: "/images/avatar.png",
+        width: 1200,
+        height: 630,
+        alt: "Raphael Sena Portfolio Image",
+      },
+    ],
+  },
 };
