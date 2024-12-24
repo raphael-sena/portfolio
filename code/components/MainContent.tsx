d56d776cbcde8cb1f6ef104743b965b4888c7ee3
raphael sena <rsenares1@gmail.com>
2024-12-24 15:43:49 -0300

    feat: add translation to projects

--- code/components/MainContent.tsx
+++ code/components/MainContent.tsx
@@ -1,15 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import FeaturedProjects from "./FeaturedProjects";
 import Footer from "./Footer";
 import Intro from "./Intro";
+import { Language, translations } from "@/services/translations";
 
-// components/MainContent.tsx
 const MainContent = () => {
+  const [language, setLanguage] = useState<Language>("en");
+  
+    useEffect(() => {
+      const savedLanguage =
+        (localStorage.getItem("language") as Language) || "en";
+      setLanguage(savedLanguage);
+    }, []);
+    
   return (
     <div>
       <Intro />
       <section id="featured-projects">
         <h2 className="text-2xl font-bold text-start mb-4">
-          Featured Projects
+          {translations[language].featured}
         </h2>
         <FeaturedProjects />
       </section>
