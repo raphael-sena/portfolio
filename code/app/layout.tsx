0a894554d276dfb2cf56f3f80cb740af640559eb
raphael sena <rsenares1@gmail.com>
2024-12-26 14:41:41 -0300

    feat: changes font

--- code/app/layout.tsx
+++ code/app/layout.tsx
@@ -22,7 +22,26 @@ export default function RootLayout({ children }: LayoutProps) {
 
   return (
     <html lang="en">
-      <body className="min-h-screen max-w-[1280px] mx-auto flex flex-col justify-center items-center bg-gradient-to-b from-slate-300 to-slate-50 text-gray-900 dark:bg-gray-900 dark:from-black dark:to-darkBlue bg-cover bg-center dark:text-slate-50">
+      {/* <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet"/>
+      </head> */}
+
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
+      <body className="min-h-screen max-w-[1280px] mx-auto flex flex-col justify-center items-center bg-gradient-to-b from-slate-200 via-slate-50 to-slate-200 text-gray-900 dark:bg-gray-900 dark:from-black dark:to-darkBlue bg-cover bg-center dark:text-slate-50">
         <header className="text-lg p-4 flex justify-between items-center w-full">
           <div className="w-full flex justify-between space-x-4">
             <ThemeToggle language={language} />
