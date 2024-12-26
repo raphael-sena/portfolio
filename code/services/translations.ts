b396b106538f6f95d50a3e34ab80e1185551eb87
raphael sena <rsenares1@gmail.com>
2024-12-26 10:44:59 -0300

    feat: add profession

--- code/services/translations.ts
+++ code/services/translations.ts
@@ -4,28 +4,45 @@ export const translations = {
   en: {
     welcome: "Welcome to my Portfolio! 😄",
     theme: "Toggle Theme",
-    intro: "Hello, I'm Raphael Sena, and welcome again to my Portfolio! Here, you'll find a selection of my projects, where I share my journey and other details to give you a glimpse of who Raphael really is 🫡. I'm always looking for new challenges and opportunities to grow as a professional. Explore my projects and feel free to get in touch! 🚀",
+    intro: "Hello, I'm Raphael, and welcome again to my Portfolio! Here, you'll find a selection of my projects, where I share my journey and other details to give you a glimpse of who Raphael really is 🫡. I'm always looking for new challenges and opportunities to grow as a professional. Explore my projects and feel free to get in touch! 🚀",
+    profession: "Software Engineer",
     footer: "Made with 🤙 by @raphael-sena",
     fork: "Fork this project!",
     featured: "Featured Projects",
     viewRepo: "View Repository",
+    about: "About",
+    about_text: "I am a Software Engineering student, and throughout my journey, I have had the opportunity to work on diverse projects, such as developing a management software for the 'Renata Senna' store and collaborating as a back-end developer on the 'Cuido Bem' project at the Experimental Software Agency at PUC Minas. These experiences allowed me to explore modern tools such as Spring Boot, Next.js, TailwindCSS, and React, as well as enhance my skills in problem-solving and delivering practical and efficient solutions. My goal is to create robust and intuitive systems that generate positive impact, valuing both technical aspects and user experience. Beyond the technical field, I aim to grow by learning new skills, such as the German language, and exploring different ways to balance my academic and professional journey with my personal life.",
+    experience: "Experience",
+    education: "Education",
   },
+
   pt: {
     welcome: "Bem-vindo(a) ao meu Portfólio! 😄",
     theme: "Alterar Tema",
-    intro: "Olá, sou o Raphael Sena e, de novo, seja muito bem vindo(a) ao meu Portfólio! Aqui, você encontrará uma seleção dos meus projetos, onde compartilho minha trajetória, além de outros detalhes para saber um pouco sobre quem é realmente o Raphael 🫡. Estou sempre em busca de novos desafios e oportunidades para crescer como profissional. Explore os projetos e sinta-se à vontade para entrar em contato! 🚀",
+    intro: "Olá, sou o Raphael e, de novo, seja muito bem vindo(a) ao meu Portfólio! Aqui, você encontrará uma seleção dos meus projetos, onde compartilho minha trajetória, além de outros detalhes para saber um pouco sobre quem é realmente o Raphael 🫡. Estou sempre em busca de novos desafios e oportunidades para crescer como profissional. Explore os projetos e sinta-se à vontade para entrar em contato! 🚀",
+    profession: "Engenheiro de Software",
     footer: "Feito com 🤙 por @raphael-sena",
     fork: "Dê um fork neste projeto!",
     featured: "Projetos em Destaque",
     viewRepo: "Ver Repositório",
+    about: "Sobre",
+    about_text: "Sou estudante de Engenharia de Software e durante minha caminhada, tive a oportunidade de trabalhar em projetos diversificados, como o desenvolvimento de um software de gestão para a loja 'Renata Senna' e minha colaboração como desenvolvedor back-end no projeto 'Cuido Bem' da Agência Experimental de Software da PUC Minas. Essas experiências me permitiram explorar ferramentas modernas como Spring Boot, Next.js, TailwindCSS e React, além de aprimorar minhas habilidades em resolver problemas e entregar soluções práticas e eficientes. Meu objetivo é criar sistemas robustos e intuitivos que gerem impacto positivo, valorizando tanto o aspecto técnico quanto a experiência do usuário. Fora do campo técnico, busco me desenvolver aprendendo novas habilidades, como o idioma alemão, e explorando diferentes formas de equilibrar minha jornada acadêmica e profissional com meu lado pessoal.",
+    experience: "Experiência",
+    education: "Educação"
   },
+  
   de: {
     welcome: "Willkommen in meinem Portfolio! 😄",
     theme: "Thema Umschalten",
-    intro: "Hallo, ich bin Raphael Sena, und herzlich willkommen wieder in meinem Portfolio! Hier finden Sie eine Auswahl meiner Projekte, in denen ich meine Reise teile und weitere Details zeige, um Ihnen einen Einblick zu geben, wer Raphael wirklich ist 🫡. Ich bin immer auf der Suche nach neuen Herausforderungen und Möglichkeiten, mich beruflich weiterzuentwickeln. Erkunden Sie meine Projekte und zögern Sie nicht, mich zu kontaktieren! 🚀",
+    intro: "Hallo, ich bin Raphael, und herzlich willkommen wieder in meinem Portfolio! Hier finden Sie eine Auswahl meiner Projekte, in denen ich meine Reise teile und weitere Details zeige, um Ihnen einen Einblick zu geben, wer Raphael wirklich ist 🫡. Ich bin immer auf der Suche nach neuen Herausforderungen und Möglichkeiten, mich beruflich weiterzuentwickeln. Erkunden Sie meine Projekte und zögern Sie nicht, mich zu kontaktieren! 🚀",
+    profession: "Softwaretechniker",
     footer: "Mit 🤙 gemacht von @raphael-sena",
     fork: "Forken Sie dieses Projekt!",
     featured: "Ausgewählte Projekte",
     viewRepo: "Repository anzeigen",
+    about: "Über",
+    about_text: "Ich bin Softwaretechnik-Student und hatte auf meinem Weg die Möglichkeit, an vielfältigen Projekten zu arbeiten, wie der Entwicklung einer Verwaltungssoftware für das Geschäft 'Renata Senna' und der Zusammenarbeit als Back-End-Entwickler am Projekt 'Cuido Bem' der Experimentellen Softwareagentur der PUC Minas. Diese Erfahrungen ermöglichten es mir, moderne Tools wie Spring Boot, Next.js, TailwindCSS und React zu erkunden und meine Fähigkeiten im Problemlösen und in der Bereitstellung praktischer und effizienter Lösungen zu verbessern. Mein Ziel ist es, robuste und intuitive Systeme zu entwickeln, die einen positiven Einfluss haben und sowohl technische Aspekte als auch die Benutzererfahrung schätzen. Abseits des technischen Bereichs strebe ich danach, mich durch das Erlernen neuer Fähigkeiten wie der deutschen Sprache weiterzuentwickeln und verschiedene Wege zu finden, meine akademische und berufliche Laufbahn mit meinem persönlichen Leben in Einklang zu bringen.",
+    experience: "Erfahrung",
+    education: "Bildung"
   },
 };
