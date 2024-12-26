1f748c6ce89e36bf7a1ddfe1b83d79ebc6125ef5
raphael sena <rsenares1@gmail.com>
2024-12-26 18:18:20 -0300

    feat: add avaso experience

--- code/components/Experience.tsx
+++ code/components/Experience.tsx
@@ -76,7 +76,8 @@ const Experience = () => {
             </Link>
           </div>
         </div>
-        <div className="ml-2">
+
+        <div className="ml-2 mb-2">
           <h2 className="mb-1 font-semibold text-md">
             {translations[language].cuido_bem_projects}
           </h2>
@@ -91,6 +92,49 @@ const Experience = () => {
               {renderCuidoBem()}
             </div>
           </div>
+        </div>
+
+        <div className="ml-2">
+          <h2 className="mb-1 font-semibold text-md">
+            {translations[language].skills}
+          </h2>
+          <ul className="flex flex-wrap" aria-label="Technologies used">
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                Java
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                Spring Boot
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                Mockito
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                SQL
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                Git
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                RestAPI
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                MVC
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
 
@@ -118,6 +162,7 @@ const Experience = () => {
               <h2 className="text-sm font-semibold">09/2021 - 09/2023</h2>
             </div>
           </div>
+
           <div className="flex items-center gap-1 justify-end">
             <Link
               href="https://www.pucminas.br/destaques/Paginas/default.aspx"
@@ -134,6 +179,147 @@ const Experience = () => {
               <FaLinkedinIn />
             </Link>
           </div>
+        </div>
+
+        <div className="ml-2 mb-2 w-full h-full">
+          <div className="flex justify-between items-center w-full">
+            <h2 className="mr-2 font-semibold text-md">
+              {translations[language].technician}
+            </h2>
+            <h2 className="text-xs font-semibold">05/2023 - 09/2023</h2>
+          </div>
+          <ul className="list-disc ml-4">
+            <li>
+              <p className="text-sm">{translations[language].technician_text}</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ml-2 mb-2 w-full h-full">
+          <div className="flex justify-between items-center w-full">
+            <h2 className="mr-2 font-semibold text-md">
+              {translations[language].technician_assistant}
+            </h2>
+            <h2 className="text-xs font-semibold">03/2022 - 04/2023</h2>
+          </div>
+          <ul className="list-disc ml-4">
+            <li>
+              <p className="text-sm">{translations[language].technician_assistant_text}</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ml-2 mb-2 w-full h-full">
+          <div className="flex justify-between items-center w-full">
+            <h2 className="mr-2 font-semibold text-md">
+              {translations[language].intern}
+            </h2>
+            <h2 className="text-xs font-semibold">09/2021 - 03/2022</h2>
+          </div>
+          <ul className="list-disc ml-4">
+            <li>
+              <p className="text-sm">{translations[language].intern_text}</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ml-2">
+          <h2 className="mb-1 font-semibold text-md">
+            {translations[language].skills}
+          </h2>
+          <ul className="flex flex-wrap" aria-label="Technologies used">
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-gray-50/10 dark:bg-gray-500/10 px-3 py-1 text-xs font-medium leading-5 dark:text-gray-500">
+                Service Desk
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-gray-50/10 dark:bg-gray-500/10 px-3 py-1 text-xs font-medium leading-5 dark:text-gray-500">
+                MS Office
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-gray-50/10 dark:bg-gray-500/10 px-3 py-1 text-xs font-medium leading-5 dark:text-gray-500">
+                User Support
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-gray-50/10 dark:bg-gray-500/10 px-3 py-1 text-xs font-medium leading-5 dark:text-gray-500">
+                Hardware Support
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-gray-50/10 dark:bg-gray-500/10 px-3 py-1 text-xs font-medium leading-5 dark:text-gray-500">
+                Active Directory
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-gray-50/10 dark:bg-gray-500/10 px-3 py-1 text-xs font-medium leading-5 dark:text-gray-500">
+                Windows
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="glass-card bg-black dark:bg-slate-50 sm:gap-2 px-6 py-4 rounded-lg text-slate-50 dark:text-black lg:opacity-60 lg:hover:opacity-100 lg:transition-opacity lg:duration-300">
+        <div className="flex mb-2">
+          <div className="w-full text-lg flex items-center mr-4 md:mr-2">
+            <Image
+              className="mr-2 transform transition-transform duration-300 hover:scale-110 rounded-md"
+              width={64}
+              height={64}
+              src="/images/experience/avaso-technology-solutions-2.jpg"
+              alt="Agência Experimental de Software"
+            />
+            <div>
+              <h1 className="text-md font-bold tracking-tighter">
+                Avaso Technology Solutions LTDA
+              </h1>
+              <h2 className="text-sm font-semibold">
+                09/2023 - {translations[language].currently}
+              </h2>
+            </div>
+          </div>
+          <div className="flex items-center  gap-1">
+            <Link
+              href="https://www.linkedin.com/company/agencia-experimental-de-software/"
+              className="transform transition-transform duration-300 hover:scale-110"
+              target="_blank"
+            >
+              <FaLinkedinIn />
+            </Link>
+          </div>
+        </div>
+
+        <div className="ml-2 mb-2">
+          <h2 className="mb-1 font-semibold text-md">
+            Field Support Engineer
+          </h2>
+          <p>{translations[language].avaso}</p>
+        </div>
+
+        <div className="ml-2">
+          <h2 className="mb-1 font-semibold text-md">
+            {translations[language].skills}
+          </h2>
+          <ul className="flex flex-wrap" aria-label="Technologies used">
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                Hardware
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                Server Maintenance
+              </div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-full bg-sky-400/10 dark:bg-sky-500/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300 dark:text-sky-400">
+                User Support
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
